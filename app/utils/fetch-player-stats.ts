180ea8f2278a3e89ebbf7636659333e490d25936
Matt Kinne <mattkinne@gmail.com>
2024-11-22 16:08:50 -0600

    Fixing projection for UTAH/Arizona

--- app/utils/fetch-player-stats.ts
+++ app/utils/fetch-player-stats.ts
@@ -1,102 +1,110 @@
-import { NHLPlayerAPI } from "../api/nhl-player.api";
-import { PlayerInfoFull, Games, PrevStats, SeasonTotals } from "../lib/nhl-player.types";
-import { RoundingService } from "./rounding-util";
+import { NHLPlayerAPI } from '../api/nhl-player.api'
+import { PlayerInfoFull, Games, PrevStats, SeasonTotals } from '../lib/nhl-player.types'
+import { RoundingService } from './rounding-util'
 
 export async function FetchPlayerStats(playerId: number) {
-    const playerProfile: PlayerInfoFull = await NHLPlayerAPI.fetchPlayerStats(playerId);
-    const games: Games = await NHLPlayerAPI.fetchPlayerMatchupStats(playerProfile.currentTeamAbbrev);
-    const prevStats: PrevStats = await NHLPlayerAPI.fetchCareerStatsVsTeams(playerId, 2);
-    const last5Games = playerProfile.last5Games;
+	const playerProfile: PlayerInfoFull = await NHLPlayerAPI.fetchPlayerStats(playerId)
+	const games: Games = await NHLPlayerAPI.fetchPlayerMatchupStats(playerProfile.currentTeamAbbrev)
+	const prevStats: PrevStats = await NHLPlayerAPI.fetchCareerStatsVsTeams(playerId, 2)
+	const last5Games = playerProfile.last5Games
 
-    // Fantasy values
-    const goalWeight = 3;
-    const assistWeight = 2;
-    const plusMinusWeight = 1;
-    const penaltyMinuteWeight = 0.1;
-    const shotsOnGoalWeight = 0.1;
-    const recentPerformanceWeight = 1.2;
+	// Fantasy values
+	const goalWeight = 3
+	const assistWeight = 2
+	const plusMinusWeight = 1
+	const penaltyMinuteWeight = 0.1
+	const shotsOnGoalWeight = 0.1
+	const recentPerformanceWeight = 1.2
 
-    let expectedWeeklyPointTotal = 0;
-    let expectedPointsPastPerformance = 0
-    let last5GamesPointToal = 0
+	let expectedWeeklyPointTotal = 0
+	let expectedPointsPastPerformance = 0
+	let last5GamesPointToal = 0
 
-    const weekProjections: SeasonTotals = {
-        goals: 0,
-        assists: 0,
-        plusMinus: 0,
-        pim: 0,
-        shots: 0,
-        points: 0,
-        gamesPlayed: 0,
-        powerPlayPoints: 0,
-        shorthandedGoals: 0,
-        shorthandedPoints: 0,
-        shorthandedAssists: 0,
-    };
+	const weekProjections: SeasonTotals = {
+		goals: 0,
+		assists: 0,
+		plusMinus: 0,
+		pim: 0,
+		shots: 0,
+		points: 0,
+		gamesPlayed: 0,
+		powerPlayPoints: 0,
+		shorthandedGoals: 0,
+		shorthandedPoints: 0,
+		shorthandedAssists: 0,
+	}
 
-    if (last5Games) {
-        last5Games.forEach((game) => {
-            const expGoals = game.goals * goalWeight;
-            const expAssists = game.assists * assistWeight;
-            const expPlusMinus = game.plusMinus * plusMinusWeight;
-            const expPenaltyMinutes = game.pim * penaltyMinuteWeight;
-            const expShots = game.shots * shotsOnGoalWeight;
+	if (last5Games) {
+		last5Games.forEach((game) => {
+			const expGoals = game.goals * goalWeight
+			const expAssists = game.assists * assistWeight
+			const expPlusMinus = game.plusMinus * plusMinusWeight
+			const expPenaltyMinutes = game.pim * penaltyMinuteWeight
+			const expShots = game.shots * shotsOnGoalWeight
 
-            const basePoints = (expGoals + expAssists + expPlusMinus + expPenaltyMinutes + expShots) * recentPerformanceWeight;
+			const basePoints =
+				(expGoals + expAssists + expPlusMinus + expPenaltyMinutes + expShots) * recentPerformanceWeight
 
-            last5GamesPointToal += basePoints;
+			last5GamesPointToal += basePoints
+		})
 
-        });
+		expectedWeeklyPointTotal += last5GamesPointToal / 5
+	}
 
-        expectedWeeklyPointTotal += (last5GamesPointToal / 5)
-    }
+	if (games && prevStats) {
+		games.forEach((game) => {
+			const teamStats =
+				game.homeTeam.abbrev === 'UTA'
+					? prevStats['ARI']
+					: prevStats[game.homeTeam.abbrev] ||
+						(game.awayTeam.abbrev === 'UTA' ? prevStats['ARI'] : prevStats[game.awayTeam.abbrev])
 
-    if (games && prevStats) {
-        games.forEach((game) => {
-            const teamStats = prevStats[game.homeTeam.abbrev] || prevStats[game.awayTeam.abbrev];
-            if (teamStats) {
-                const expGoals = (teamStats.goals / teamStats.gamesPlayed) * goalWeight;
-                const expAssists = (teamStats.assists / teamStats.gamesPlayed) * assistWeight;
-                const expPlusMinus = (teamStats.plusMinus / teamStats.gamesPlayed) * plusMinusWeight;
-                const expPenaltyMinutes = (teamStats.pim / teamStats.gamesPlayed) * penaltyMinuteWeight;
-                const expShots = (teamStats.shots / teamStats.gamesPlayed) * shotsOnGoalWeight;
-                const expPowerPlayPoints = (teamStats.powerPlayPoints / teamStats.gamesPlayed);
-                const expShorthandedGoals = (teamStats.shorthandedGoals / teamStats.gamesPlayed);
-                const expShorthandedAssists = ((teamStats.shorthandedPoints - teamStats.shorthandedGoals) / teamStats.gamesPlayed);
+			if (teamStats) {
+				const expGoals = (teamStats.goals / teamStats.gamesPlayed) * goalWeight
+				const expAssists = (teamStats.assists / teamStats.gamesPlayed) * assistWeight
+				const expPlusMinus = (teamStats.plusMinus / teamStats.gamesPlayed) * plusMinusWeight
+				const expPenaltyMinutes = (teamStats.pim / teamStats.gamesPlayed) * penaltyMinuteWeight
+				const expShots = (teamStats.shots / teamStats.gamesPlayed) * shotsOnGoalWeight
+				const expPowerPlayPoints = teamStats.powerPlayPoints / teamStats.gamesPlayed
+				const expShorthandedGoals = teamStats.shorthandedGoals / teamStats.gamesPlayed
+				const expShorthandedAssists =
+					(teamStats.shorthandedPoints - teamStats.shorthandedGoals) / teamStats.gamesPlayed
 
-                let basePoints = expGoals + expAssists + expPlusMinus + expPenaltyMinutes + expShots;
+				let basePoints = expGoals + expAssists + expPlusMinus + expPenaltyMinutes + expShots
 
-                if (playerProfile.position === "D") {
-                    const totalPoints = (teamStats.goals + teamStats.assists) / teamStats.gamesPlayed; // Calculate points per game
-                    basePoints += totalPoints; // Add +1 per point
-                }
+				if (playerProfile.position === 'D') {
+					const totalPoints = (teamStats.goals + teamStats.assists) / teamStats.gamesPlayed // Calculate points per game
+					basePoints += totalPoints // Add +1 per point
+				}
 
-                expectedPointsPastPerformance = basePoints + expPowerPlayPoints + expShorthandedGoals + expShorthandedAssists;
-                expectedWeeklyPointTotal += expectedPointsPastPerformance;
+				expectedPointsPastPerformance =
+					basePoints + expPowerPlayPoints + expShorthandedGoals + expShorthandedAssists
+				expectedWeeklyPointTotal += expectedPointsPastPerformance
 
-                // Update weekProjections
-                weekProjections.goals += teamStats.goals / teamStats.gamesPlayed;
-                weekProjections.assists += teamStats.assists / teamStats.gamesPlayed;
-                weekProjections.plusMinus += teamStats.plusMinus / teamStats.gamesPlayed;
-                weekProjections.pim += teamStats.pim / teamStats.gamesPlayed;
-                weekProjections.shots += teamStats.shots / teamStats.gamesPlayed;
-                weekProjections.points += (teamStats.goals + teamStats.assists) / teamStats.gamesPlayed;
-                weekProjections.gamesPlayed += 1;
-                weekProjections.powerPlayPoints += teamStats.powerPlayPoints / teamStats.gamesPlayed;
-                weekProjections.shorthandedGoals += teamStats.shorthandedGoals / teamStats.gamesPlayed;
-                weekProjections.shorthandedAssists += (teamStats.shorthandedPoints - teamStats.shorthandedGoals) / teamStats.gamesPlayed;
-            }
-        });
-    }
+				// Update weekProjections
+				weekProjections.goals += teamStats.goals / teamStats.gamesPlayed
+				weekProjections.assists += teamStats.assists / teamStats.gamesPlayed
+				weekProjections.plusMinus += teamStats.plusMinus / teamStats.gamesPlayed
+				weekProjections.pim += teamStats.pim / teamStats.gamesPlayed
+				weekProjections.shots += teamStats.shots / teamStats.gamesPlayed
+				weekProjections.points += (teamStats.goals + teamStats.assists) / teamStats.gamesPlayed
+				weekProjections.gamesPlayed += 1
+				weekProjections.powerPlayPoints += teamStats.powerPlayPoints / teamStats.gamesPlayed
+				weekProjections.shorthandedGoals += teamStats.shorthandedGoals / teamStats.gamesPlayed
+				weekProjections.shorthandedAssists +=
+					(teamStats.shorthandedPoints - teamStats.shorthandedGoals) / teamStats.gamesPlayed
+			}
+		})
+	}
 
-    expectedWeeklyPointTotal = ((expectedWeeklyPointTotal / 4) * 3)
+	expectedWeeklyPointTotal = (expectedWeeklyPointTotal / 4) * 3
 
-    return {
-        playerProfile,
-        games,
-        prevStats,
-        last5Games,
-        expectedWeeklyPointTotal: RoundingService.roundToDecimal(expectedWeeklyPointTotal, 2),
-        weekProjections,
-    };
+	return {
+		playerProfile,
+		games,
+		prevStats,
+		last5Games,
+		expectedWeeklyPointTotal: RoundingService.roundToDecimal(expectedWeeklyPointTotal, 2),
+		weekProjections,
+	}
 }
