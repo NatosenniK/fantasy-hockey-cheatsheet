6bc5646684d0e8631943995ada27bd98e131b9f0
Matt Kinne <mattkinne@gmail.com>
2025-01-05 19:05:36 -0600

    Removing unused code

--- app/ui/trade-calculator/trade-calculator.tsx
+++ app/ui/trade-calculator/trade-calculator.tsx
@@ -6,8 +6,6 @@ import { PlayerProfile } from '@/app/lib/api/external/nhl/nhl-player.types'
 import { GeminiAPI } from '@/app/lib/api/external/gemini/gemini-ai.api'
 import TradeOverview from './trade-overview'
 
-type PreferredSide = 'outgoing' | 'incoming' | 'indeterminate'
-
 export default function TradeCalculator() {
 	const [tradeSideAlpha, setTradeSideAlpha] = useState<PlayerProfile[]>([])
 	const [tradeSideBeta, setTradeSideBeta] = useState<PlayerProfile[]>([])
